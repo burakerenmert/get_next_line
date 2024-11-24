f1c330d37ed9e62e72e6fae7c31990d660859498
Burak Eren Mert <buramert@k1m43s02.42istanbul.com.tr>
2024-11-25 00:42:35 +0300

    burak

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,47 @@
 /*   By: buramert <buramert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:56:15 by buramert          #+#    #+#             */
-/*   Updated: 2024/11/24 22:28:39 by buramert         ###   ########.fr       */
+/*   Updated: 2024/11/25 00:28:25 by buramert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-// void ft_free(str)
-// {
+char *ft_parser(char *str, int line)
+{
+	int i;
+	char *str_line;
 	
-// }
-// char *ft_check_line(str)
-// {
-// 	int i;
+	i = 0;
+	str_line = malloc(sizeof(char) * (line + 2));
+	while(i < line)
+	{
+		str_line[i] = str[i];
+		i++;
+	}
+	if(str[i] == '\n')
+	{
+		str_line[i] == '\n';
+		i++;
+	}
+	str_line[i] = '\0';
+	return(str_line);
+}
+int ft_check_line(char *str)
+{
+	int i;
 	
-// 	i = 0;
-// }
+	i = 0;
+	while(str[i] && str[i] != '\n')
+	{
+		i++;
+	}
+	if(str[i] == '\n')
+	{
+		return(i);
+	}
+	else
+		return(0);
+}
 char *ft_read(int fd, char *str)
 {
 	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
@@ -29,9 +55,16 @@ char *ft_read(int fd, char *str)
 }
 char *get_next_line(int fd)
 {
+	int			line;
 	static char *str;
+	char *line_to_return;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+        return NULL;
 	str = ft_read(fd, str);
-	printf("%s", str);
+	line = ft_check_line(str);
+	if(line != 0)
+		line_to_return = ft_parser(str, line);
 	return(str);
 }
 int main()
